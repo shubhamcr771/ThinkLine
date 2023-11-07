4065a0c17842ef140890b29cc31b84cd76ddfd0b
Shubham <shubhamsingh705010@gmail.com>
2023-11-07 11:36:14 +0530

    done

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Create an array to store blog posts (since you're not using a database).
 const blogPosts = [];
